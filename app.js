db8678c92e578b0e98fa9bc3ea936f52e2825c25
carotene512 <karoten512@gmail.com>
2018-04-26 22:46:56 +0900

    一旦マーカが表示されるようにした。

--- app.js
+++ app.js
@@ -8,38 +8,45 @@
     ZOOM: 'zoom'
   };
 
-  /** コンストラクタ */
-  var TMap = function() {
+  var TMap = function(element) {
+    this.markers = [];
+    this.initMap(element);
+    this.initMarker(element);
+  };
 
-    /** private variables */
-    var TMap = function(element) {
-      this.initMap(element);
-      this.initMarker(element);
-    };
+  /** mapの初期化を行う */
+  TMap.prototype.initMap = function(element) {
+    var lat = $(element).data(DataKey.LAT);
+    var lng = $(element).data(DataKey.LNG);
+    var zoom = $(element).data(DataKey.ZOOM);
 
-    /** mapの初期化を行う */
-    TMap.prototype.initMap = function(element) {
-      var lat = $(element).data(DataKey.LAT);
-      var lng = $(element).data(DataKey.LNG);
-      var zoom = $(element).data(DataKey.ZOOM);
-      this.map = new google.maps.Map(element, {
-        center: {lat: lat, lng: lng},
-        zoom: zoom
-      });
-    }
+    this.map = new google.maps.Map(element, {
+      center: {lat: lat, lng: lng},
+      zoom: zoom
+    });
+  }
 
+  TMap.prototype.initMarker = function(element) {
+    var mapId = $(element).attr('id');
+    var self = this;
+    $('[data-target="' + mapId + '"').each(function() {
+      var lat = $(this).data(DataKey.LAT);
+      var lng = $(this).data(DataKey.LNG);
 
-    /** コンストラクタを返す */
-    return TMap;
-  }();
-
+      self.markers.push(
+        new google.maps.Marker({
+          position: {lat: lat, lng: lng},
+          map: self.map,
+        })
+      );
+    });
+  }
 
   /** JavaScriptを書かずにmapとmarkerを表示する */
   $(document).ready(function() {
     $(Selector).each(function() {
-      /** それぞれのelementに対してmapの初期化を行う */
-      new TMap(this);
+      /** それぞれのelementに対してmapの初期化を行い、オブジェクトとして持たせる */
+      $(this).data('t.map', new TMap(this));
     });
   });
-
 })();
