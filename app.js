1c3f414b0c89b1f804a63385ad5873f960b8b1b2
carotene512 <karoten512@gmail.com>
2018-04-26 22:25:38 +0900

    map表示の初期化をmapInit methodにまとめた。

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
 (function() {
+
   var Selector = '[data-toggle="map"]';
+
+  var DataKey = {
+    LAT: 'lat',
+    LNG: 'lng',
+    ZOOM: 'zoom'
+  };
+
+  /** コンストラクタ */
+  var TMap = function() {
+
+    /** private variables */
+    var TMap = function(element) {
+      this.initMap(element);
+      this.initMarker(element);
+    };
+
+    /** mapの初期化を行う */
+    TMap.prototype.initMap = function(element) {
+      var lat = $(element).data(DataKey.LAT);
+      var lng = $(element).data(DataKey.LNG);
+      var zoom = $(element).data(DataKey.ZOOM);
+      this.map = new google.maps.Map(element, {
+        center: {lat: lat, lng: lng},
+        zoom: zoom
+      });
+    }
+
+
+    /** コンストラクタを返す */
+    return TMap;
+  }();
 
 
   /** JavaScriptを書かずにmapとmarkerを表示する */
   $(document).ready(function() {
     $(Selector).each(function() {
-
-      /** mapの初期化をする */
-      var lat = $(this).data('lat');
-      var lng = $(this).data('lng');
-      var zoom = $(this).data('zoom');
-
-      map = new google.maps.Map(this, {
-        center: {lat: lat, lng: lng},
-        zoom: zoom
-      });
-
+      /** それぞれのelementに対してmapの初期化を行う */
+      new TMap(this);
     });
-  })
+  });
 
 })();
